6c102da029e555848803e7d4ded9e7ffacc20f5e
Toxik24 <samuel.pittonchavo@gmail.com>
2023-12-05 10:40:52 +0100

    ex 1 c#

---
+++ Exercices/C#/Ex1_Csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ex1_Csharp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+
+
+
+
+
+        }
+    }
+}
