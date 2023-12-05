afa8f8eac7a648f4b20e5d467248bc9f793a9000
Toxik24 <samuel.pittonchavo@gmail.com>
2023-12-05 10:18:09 +0100

    demo C#

---
+++ Exercices/C#/Demo_SLO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SLO // englobe tout les fichier ensemble
+{
+
+    class Program
+    {
+        // attributs
+
+        // methode
+
+        // "fonction"
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
