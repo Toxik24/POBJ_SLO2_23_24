5db2d6b36cd0b9fc882956d1e744e0b23f194434
Toxik24 <samuel.pittonchavo@gmail.com>
2023-12-08 11:00:51 +0100

    refrech

---
+++ Exercices/C#/Demo_SLO/Program-DESKTOP-K564540.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SLO // englobe tout les fichier ensemble
+{
+
+    class Program
+    {
+        // attributs
+
+        // methode
+
+        // "fonction"
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            // type primmitif
+            // entier 
+            bool valbo = false;
+            int valD = 0;
+            char ValC = 'A';
+            short Vals = 0;
+            long ValL = 0;
+
+
+            //réel
+            float valF = 0.0f;
+            double ValR = 0;
+
+            //objet
+            string  vals = null;
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
