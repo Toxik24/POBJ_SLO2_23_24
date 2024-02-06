b1f29ce0f5c8e8b8c4e34f9ed92fb2b862506719
Toxik24 <samuel.pittonchavo@gmail.com>
2024-02-06 11:26:03 +0100

    code C# pas totalement fonctionnel
    bug affichage des valeur et recuperation des valeurs

---
+++ Exercices/C#/EX4_CS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4_CS
+{
+    class Programe
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Utilisateur UserA = new Utilisateur();
+            Utilisateur UserB = new Utilisateur();
+            Utilisateur UserC = new Utilisateur();
+
+            UserA.ChoisirForme(0);
+            UserA.SaisirLesParametres();
+            UserA.ExecuterLesCalculs();
+            UserA.AfficherResultats();
+
+
+            UserB.ChoisirForme(1);
+            UserB.SaisirLesParametres();
+            UserB.ExecuterLesCalculs();
+            UserB.AfficherResultats();
+
+            UserC.ChoisirForme(2);
+            UserC.SaisirLesParametres();
+            UserC.ExecuterLesCalculs();
+            UserC.AfficherResultats();
+
+        }
+    }
+}
+
+
